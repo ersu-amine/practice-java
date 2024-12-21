fadee50c4f87b04885d887cedb66f8ab7172e988
ersu-amine <ersu-amine@gmail.com>
2024-12-20 22:46:42 -0500

    add RemoveDuplicates task second challenge and its solution

--- src/test/java/com/project/leetcode_top150/RemoveDuplicates.java
+++ src/test/java/com/project/leetcode_top150/RemoveDuplicates.java
@@ -9,9 +9,9 @@ public class RemoveDuplicates {//from sorted array
     public int removeDuplicates(int[] nums) {
         //ordered array, max number is the last one
         // mark duplicate ones as the biggest number
-        int max = nums[nums.length-1] +1;
+        int max = nums[nums.length - 1] + 1;
 
-        //keep track of how many duplicates were there
+        //keep track of how many uniques were there
         int size = nums.length;
 
         //iterate and set each duplicated number as the max number
@@ -26,16 +26,48 @@ public class RemoveDuplicates {//from sorted array
         System.out.println("Arrays.toString(nums) = " + Arrays.toString(nums));
 
         return size;
+    }
 
+    //leaving 2 of each
+    public int removeDuplicates2(int[] nums) {
+        //ordered array, max number is the last one
+        // mark duplicate ones as the biggest number
+        int max = nums[nums.length - 1] + 1;
+
+        //keep track of how many uniques were there
+        int size = nums.length;
+
+        //iterate and set each duplicated number as the max number, leave doubles, triples not allowed
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) {
+                nums[i] = max;
+                size--;
+            }
+        }
+        //duplicates are moved to the left after sort
+        Arrays.sort(nums);
+        System.out.println("Arrays.toString(nums) = " + Arrays.toString(nums));
+
+        //size of array that corresponds to the unique part
+        return size;
     }
 
     @Test
     void test1() {
-        removeDuplicates(new int[]{1, 1, 2,});
+        int i = removeDuplicates(new int[]{1, 1, 2,});
+        System.out.println("i = " + i);
+
+        int j = removeDuplicates2(new int[]{1, 1, 1, 2, 2, 3});
+        System.out.println("j = " + j);
     }
 
     @Test
     void test2() {
-        removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
+        int i = removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4});
+
+        System.out.println("i = " + i);
+
+        int j = removeDuplicates2(new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3});
+        System.out.println("j = " + j);
     }
 }
