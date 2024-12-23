fcccbf5891a5c16eb2670513d1ab48473aba5716
ersu-amine <ersu-amine@gmail.com>
2024-12-22 23:29:43 -0500

    add Library text based UI

--- src/test/java/com/project/tasks/library_management/LibraryRunner.java
+++ src/test/java/com/project/tasks/library_management/LibraryRunner.java
@@ -12,12 +12,30 @@ public class LibraryRunner {
                 "   5. Exit\n");
         int choice;
         Scanner scan = new Scanner(System.in);
-        do{
+        do {
             System.out.println("   Enter your choice: ");
             choice = scan.nextInt();
 
-        } while (choice!=5);
+            switch (choice) {
+                case 1:
+                    System.out.println("Please enter the title of the book: ");
+                    String title = scan.nextLine();
+                    System.out.println("Please enter the author of the book: ");
+                    String author = scan.nextLine();
+                    System.out.println("Please enter the isbn of the book: ");
+                    String isbn = scan.nextLine();
+                    Library.addBook(new Book(title, author, isbn));
+                    break;
+                    //TODO borrow book
+//                case 2:
+//                    System.out.println("Please enter the title of the book you want to borrow: ");
+//                    String bookToBorrow = scan.nextLine();
+//                    Library.borrowBook();
+            }
 
+        } while (choice != 5);
+
+        System.out.println("Bye bye");
 
 
     }
