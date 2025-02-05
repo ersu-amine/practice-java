b475620fe0db1d9fc9e5e30e6853f28f789f5187
ersu-amine <ersu.amine@gmail.com>
2025-02-04 23:59:09 -0500

    Number: FizzBuzz setup

---
+++ src/test/java/com/project/numbers/FizzBuzz.java
@@ -0,0 +1,10 @@
+package com.project.numbers;
+
+public class FizzBuzz {
+
+    public String [] fizzBuzz(int n){
+        //make empty array to store numbers and words, Fizz, Buzz, or FizzBuzz
+        String[] array = new String[n];
+
+    }
+}
